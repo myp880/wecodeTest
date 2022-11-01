d8eb12b63723084c6143773d2d33672720c2869a
myp <myp880@gmail.com>
2022-11-01 10:16:32 +0900

    react update

---
+++ westa/src/pages/Login/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "../Login/login.css"
+import {Link, useNavigate} from 'react-router-dom'
+
+const Login = () => {
+    const navigate = useNavigate();
+
+    const goTomain = () => {
+        if ( Response.message === "valid user" ) {
+            navigate("/main");
+        } else {
+            alert("가입된 회원이 아닙니다.");
+            navigate("/signup");
+        }
+    }
+
+
+    return (
+    <div className="loginWrap">
+        <div className="loginArea">
+            {/* <h1 className="logo"><a href="">instagram</a></h1> */}
+            <h1 className="logo"><Link>westagram</Link></h1>
+            <div className="inputDataArea">
+                <div className="loginFormBox"><input className="userId" type="text" placeholder="전화번호, 사용자 이름 또는 이메일"/></div>
+                <div className="loginFormBox"><input className="userPw" type="password" placeholder="비밀번호"/></div>
+                <button className="btnLogin" disabled="disabled" onClick={goTomain} >로그인</button>
+            </div>
+
+            <a href="" className="forgotPw">비밀번호를 잊으셨나요?</a>
+        </div>
+    </div>
+    );
+};
+
+export default Login;
